3ab4fcd97bcaf491bae35350513a2a5f0ce9b77f
Kevin Kuch <kuchkevin96@gmail.com>
2023-02-13 15:03:59 -0500

    s4c1 - Ajout style des articles

--- front-page.php
+++ front-page.php
@@ -7,6 +7,9 @@
 <?php get_header(); ?>
 <main>
     <h3>front-page.php</h3>
+    <div>
+        <img src="./medias/patternpad.png" alt="test">
+    </div>
         <section class="blocFlex">
             <?php 
                 if(have_posts()) : 
