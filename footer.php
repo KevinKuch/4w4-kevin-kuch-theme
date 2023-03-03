bf6d28adef7fe690cce5c848c6bea4c3c765eb3c
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-03 08:42:09 -0500

    s6c2 - correction mise en page footer et menu secondaire

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
-<footer class="site_footer">
-    <section>Colonne 1</section>
+<footer class="site__footer">
+    <?php the_custom_logo(); ?>
+    <section>À propos 
+        <ul>
+            <li>test1</li>
+            <li>test2</li>
+            <li>test3</li>
+        </ul>
+    </section>
     <section>© Automne Hiver 2023 Kevin Kuch</section>
-    <section>Colonne 3</section>
+    <section>Médias sociaux
+        <ul>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+        </ul>
+    </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
