b7823b80c280c39633e16017b96c447264dc08f9
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-10 08:57:16 -0500

    s8c2 - retirer le menu aside au front-page

--- header.php
+++ header.php
@@ -7,7 +7,14 @@
     <?php wp_head(); ?>
 </head>
 
-<body class="site">
+<?php 
+$nouvelle_classe = "";
+if(is_front_page()){
+    $nouvelle_classe = "no-aside";
+}
+?>
+
+<body class="site <?= $nouvelle_classe ?>">
    <header class="site__header">
     <section class="site__header__logo">
         <div class="logomenu">
@@ -23,10 +30,9 @@
     <h1 class="titre-web">Création d'interface et Développement Web</h1>
    </header>
   
-<aside class="site__aside">
-    <h3>Menu secondaire</h3>
-    <?php wp_nav_menu(array(
-        "menu" => "aside",
-        "container" => "nav"  
-    )); ?>
+   <?php 
+    if ( ! is_front_page()){
+    get_template_part("template-parts/aside"); 
+    }
+    ?>
 </aside>
