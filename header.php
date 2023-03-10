6cb9add16988dee18177b8d68694cc52efa0c520
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-10 10:43:52 -0500

    s7c2 - correction display du burger

--- header.php
+++ header.php
@@ -6,35 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-
 <?php 
-$nouvelle_classe = "";
-if(is_front_page()){
-    $nouvelle_classe = "no-aside";
-}
+/*
+  $nouvelle_classe = ""
+  if (is_front_page()){
+    $nouvelle_classe = 'no-aside';
+  }
+*/
 ?>
+<!-- body class="site  // $nouvelle_classe " -->
+<body class="site <?php echo (is_front_page()?'no-aside':'');?>">
 
-<body class="site <?= $nouvelle_classe ?>">
    <header class="site__header">
     <section class="site__header__logo">
-        <?php the_custom_logo(); ?>
-        <div class="logomenu">
+      <?php the_custom_logo(); ?>
+      <div class="menu__recherche">
             <input type="checkbox" id="chkMenu">
             <?php wp_nav_menu(array(
             "menu" => "entete",
             "container" => "nav"
             )) ?>
-        </div>
-        <?php  get_search_form(); ?>
-        <label class="burger" for="chkMenu"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu" alt="menu" width="32" height="32"></label>
+        
+            <?php  get_search_form(); ?>
+            <label class="burger" for="chkMenu"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="20" height="20"></label>
+           
+      </div>  
     </section>
     <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
-    <h1 class="titre-web">Création d'interface et Développement Web</h1>
+    <h2><?= bloginfo('description') ?></h2>
    </header>
-  
-   <?php 
+
+    <?php 
     if ( ! is_front_page()){
     get_template_part("template-parts/aside"); 
     }
     ?>
-</aside>
